5349b3e7456ff4dab6ec58f94e80840c7e80d5b7
LadyKerr <kadiekerr@gmail.com>
2019-07-10 16:04:49 -0400

    logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require("./server.js");
+
+const port = 5000;
+server.listen(port, () =>
+  console.log(`\n***server is running on port ${port}***\n`)
+);
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = 'express';
+const express = require("express");
+
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
-
 //custom middleware
-
 function logger(req, res, next) {
+  const timestamp = Date.now();
+  console.log(`${req.method} to ${req.baseUrl} at ${timestamp}`);
 
-};
+  next();
+}
+
+//global middleware setup
+server.use(logger);
+server.use(express.json());
+
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
