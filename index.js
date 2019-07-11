46985d58c3891e142aac69448fa416ffc236491f
LadyKerr <kadiekerr@gmail.com>
2019-07-11 14:02:34 -0400

    added port env

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const server = require("./server.js");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () =>
-  console.log(`\n***server is running on port ${port}***\n`)
+  console.log(`\n***server is running on http://localhost:${port} ***\n`)
 );
